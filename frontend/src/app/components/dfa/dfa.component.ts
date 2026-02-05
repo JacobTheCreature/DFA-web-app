a10fa9b4ff8f0fccd3c3951aee66beb3874d3006
Jacob Schirmer <Jacobmschirmer01@gmail.com>
2026-02-05 13:07:49 -0900

    make some basic components and service

---
+++ frontend/src/app/components/dfa/dfa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dfa.component',
+  imports: [],
+  templateUrl: './dfa.component.html',
+  styleUrl: './dfa.component.css',
+})
+export class DfaComponent {
+
+}
---
+++ frontend/src/app/components/transition-table/transition-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transition-table.component',
+  imports: [],
+  templateUrl: './transition-table.component.html',
+  styleUrl: './transition-table.component.css',
+})
+export class TransitionTableComponent {
+
+}
